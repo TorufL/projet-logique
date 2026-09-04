8835450d8c2f1b5fd594e33ce44c19eea62b4638
6316350 <lolilan340@gmail.com>
2026-09-04 16:45:34 -0400

    yesser

---
+++ s3/ex2.py
@@ -0,0 +1,26 @@
+#for
+
+n =5
+
+for i in range(1, n + 1):
+    for j in range(1):
+        print("*", end=" ")
+    print( )
+
+print(" ")
+
+#while
+
+x = 5
+
+y = 1
+
+while y <= x:
+    z = 1
+
+    while z <= y:
+        print("*", end=(" "))
+        z += 1
+
+    print()
+    y += 1
