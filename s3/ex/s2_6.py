86f3bc78f69d356a0aa28d071f1c12184e7d1fd4
6316350 <lolilan340@gmail.com>
2026-09-11 16:55:03 -0400

    yesser

---
+++ s3/ex/s2_6.py
@@ -0,0 +1,28 @@
+#
+#Programme:analyse de la catégorie et droits de la personne
+#
+
+# TODO: Début
+# TODO: Créer la fonction calculer_age(annee_naissance)
+# TODO: Définir annee_actuelle
+# TODO: Retourner age = anne_actuelle - annee_naissance
+#
+# TODO: créer la fonction determiner_categorie(age)
+# TODO: Si age >= 18
+# TODO:     la catégorie est "adulte"
+# TODO: sinon si age >= 13
+# TODO:     la catégorie est "adolescent"
+# TODO: sinon
+# TODO:     la catégorie est "enfant"
+#
+# TODO: céer la fonction afficher_droits(age)
+# TODO: si age >= 18
+# TODO:     afficher: "x droit de vote"
+# TODO: sinon
+# TODO:     afficher: "- droit de vote"
+# TODO: si age >= 5 et age < 50
+# TODO:     afficher: "X etre à l'école"
+# TODO: sinon
+# TODO:     afficher: "- etre à l'école"
+# TODO: si age < 18 ou age >= 65
+# TODO:
